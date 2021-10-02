c2c15e567f465001c281c9e435b57964306649f0
pratikdk <pratik.deoolwadikar@gmail.com>
2021-10-02 22:54:20 +0530

    intialized project, components, and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ Home }/>
+          <Route path="/about" component={ About }/>
+          <Route path="/contact" component={ Contact }/>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis lobortis nibh. Curabitur tempus justo sed sollicitudin commodo. Phasellus vel feugiat odio. Ut dictum, ex ut pulvinar molestie, nibh orci vehicula nisl, in aliquet tellus arcu sit amet libero. Quisque rhoncus pellentesque est. Vivamus sodales auctor elit in facilisis. Pellentesque euismod turpis a lorem faucibus, sed faucibus turpis sodales. Praesent vitae commodo est. Suspendisse potenti. Etiam nisi nisl, luctus a urna sed, feugiat iaculis augue. Praesent consequat ultrices turpis, feugiat rhoncus metus hendrerit id. Maecenas vel dapibus augue, nec gravida purus. Pellentesque faucibus tempus tincidunt. Vivamus ut felis vel purus accumsan faucibus. Praesent ultricies eros ac magna interdum luctus. Sed suscipit sed neque ut eleifend. Sed a erat nulla. Cras pellentesque eget nunc eget aliquet. Sed felis neque, congue ac leo eu, mattis porttitor neque. Vivamus viverra mauris eget aliquam aliquet. Sed pulvinar orci non tortor feugiat lacinia. Vestibulum consequat non urna non posuere. Nunc efficitur nulla dapibus elit sodales feugiat sit amet non sapien. Curabitur et orci ac nunc molestie dictum. Etiam et congue ipsum. Nulla rhoncus eleifend arcu non lobortis.</p>
+        </div>
+    )
+}
+export default Home;
