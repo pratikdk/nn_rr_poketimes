a4abe421b30f0228b2ec560b8d2146bb96f2585f
pratikdk <pratik.deoolwadikar@gmail.com>
2021-10-03 00:48:52 +0530

    commiting missed changes

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rainbow from "../hoc/Rainbow";
 
 const About = (props) => {
     // setTimeout(() => {
@@ -11,4 +12,4 @@ const About = (props) => {
         </div>
     );
 }
-export default About;
+export default Rainbow(About);
