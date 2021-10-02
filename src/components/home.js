eef5dd17b07eac4baf8a1b4c1b289891ee8810f0
pratikdk <pratik.deoolwadikar@gmail.com>
2021-10-03 02:46:52 +0530

    Added get query to placeholder api to list array response in home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,41 @@
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis lobortis nibh. Curabitur tempus justo sed sollicitudin commodo. Phasellus vel feugiat odio. Ut dictum, ex ut pulvinar molestie, nibh orci vehicula nisl, in aliquet tellus arcu sit amet libero. Quisque rhoncus pellentesque est. Vivamus sodales auctor elit in facilisis. Pellentesque euismod turpis a lorem faucibus, sed faucibus turpis sodales. Praesent vitae commodo est. Suspendisse potenti. Etiam nisi nisl, luctus a urna sed, feugiat iaculis augue. Praesent consequat ultrices turpis, feugiat rhoncus metus hendrerit id. Maecenas vel dapibus augue, nec gravida purus. Pellentesque faucibus tempus tincidunt. Vivamus ut felis vel purus accumsan faucibus. Praesent ultricies eros ac magna interdum luctus. Sed suscipit sed neque ut eleifend. Sed a erat nulla. Cras pellentesque eget nunc eget aliquet. Sed felis neque, congue ac leo eu, mattis porttitor neque. Vivamus viverra mauris eget aliquam aliquet. Sed pulvinar orci non tortor feugiat lacinia. Vestibulum consequat non urna non posuere. Nunc efficitur nulla dapibus elit sodales feugiat sit amet non sapien. Curabitur et orci ac nunc molestie dictum. Etiam et congue ipsum. Nulla rhoncus eleifend arcu non lobortis.</p>
-        </div>
-    )
+import React from "react";
+import axios from "axios";
+
+class Home extends React.Component {
+    state = {
+        posts: []
+    }
+    componentDidMount = () => {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => {
+                console.log(res);
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+    }
+    render() { 
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card">
+                        <div className="card-content">
+                            <span className="card-title">{ post.title }</span>
+                            <p>{ post.body }</p>
+                        </div>
+                    </div>
+                );
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        );
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                { postList }
+            </div>
+        )
+    }
 }
 export default Home;
