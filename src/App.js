7c2ec18508d54d3491890d90f92b6b052846da88
pratikdk <pratik.deoolwadikar@gmail.com>
2021-10-03 00:20:18 +0530

    added Link components to navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
